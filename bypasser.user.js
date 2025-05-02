1fc3167f84363d0b08ebe0d229558a0b0f0e7ef0
Bronny James <209851802+LeBronsGrandson@users.noreply.github.com>
2025-05-01 23:25:00 -0400

    oka

---
+++ bypasser.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         swift key bypass
+// @namespace    https://discord.gg/v2nmK8jNef
+// @version      6.9
+// @description  skip past ads required by swift's key system
+// @author       official.darian and emma.5829
+// @match        https://key.getswift.gg/*
+// @match        https://loot-link.com/*
+// @grant        GM.xmlHttpRequest
+// @require      https://raw.githubusercontent.com/LeBronsGrandson/citamSaveUs/refs/heads/main/peytonLikesMen.js
+// ==/UserScript==
+
+/*
+RAPED ROBLOX EXECUTORS - discord.gg/v2nmK8jNef
+RAPED ROBLOX EXECUTORS - discord.gg/v2nmK8jNef
+RAPED ROBLOX EXECUTORS - discord.gg/v2nmK8jNef
+*/
+
+/* 
+     _          _                              _                  _ ____
+  __| |__ _ _ _(_)__ _ _ _   __ __ ____ _ ___ | |_  ___ _ _ ___  (_)__ /
+ / _` / _` | '_| / _` | ' \  \ V  V / _` (_-< | ' \/ -_) '_/ -_)  _ |_ \
+ \__,_\__,_|_| |_\__,_|_||_|  \_/\_/\__,_/__/ |_||_\___|_| \___| (_)___/
+*/
